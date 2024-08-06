3dd7ad51774ac6f65d72366727190d710abb79a8
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-07 00:45:14 +0300

    Add project files.

---
+++ PatikaWeek4Homework1/Persons.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4Homework1
+{
+    public class Persons
+    {
+        // Fields oluşturuluyor
+        private string firstName;
+        private string lastName;
+        private DateTime birthDate;
+
+        // Properties oluşturuluyor
+        public string FirstName
+        {
+            get
+            { 
+                return firstName; 
+            }
+            set
+            {
+                firstName = value;
+            }
+                
+        }
+        public string LastName
+        {
+            get {
+                return lastName; 
+            }
+            set
+            {
+               lastName = value;
+            }
+        }
+        
+        public DateTime BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+            set
+            { birthDate = value; }
+        }
+
+
+        //Person bilgileri yazdırılıyor
+        public void PersonsPrint()
+        {
+            Console.WriteLine($" Ad: {FirstName}\n Soyadı: {LastName} \n Doğum Tarihi: {BirthDate.ToShortDateString()} \n\n");
+
+        }
+    }
+}
---
+++ PatikaWeek4Homework1/Program.cs
@@ -0,0 +1,35 @@
+﻿using PatikaWeek4Homework1;
+
+//-----------Öğrenci nesneleri --------------
+Console.WriteLine("---------Öğrenciler--------------");
+
+Persons student1 = new Persons();
+student1.FirstName = "Gamze";
+student1.LastName = "Tekir";
+student1.BirthDate = new DateTime(2012,5,22);
+student1.PersonsPrint();
+
+Persons student2 = new Persons() {
+
+    FirstName = "Serkan",
+    LastName = "Dilgen",
+    BirthDate = new DateTime(2010, 11, 2)
+};
+student2.PersonsPrint();
+
+//-----------Öğretmen nesneler--------------
+Console.WriteLine("---------Öğretmenler--------------");
+
+Persons teacher1 = new Persons();
+teacher1.FirstName = "Serap";
+teacher1.LastName = "Gökova";
+teacher1.BirthDate = new DateTime(1990, 6, 5);
+teacher1.PersonsPrint();
+
+Persons teacher2 = new Persons
+{
+    FirstName = "Ahmet",
+    LastName = "Yıldırım",
+    BirthDate = new DateTime(1985, 10, 14)
+};
+teacher2.PersonsPrint();
